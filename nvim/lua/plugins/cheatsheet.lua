ebeb5f0080607cd6d0b4d90c1f7a18c75d4eee68
Christian C. Berclaz <christian.berclaz@mac.com>
2024-11-12 20:14:50 +0100

    CHANGED: NVIM Changed the shortcut for Cheatsheet to <localleader>? to remove the conflict with which-key.

--- nvim/lua/plugins/cheatsheet.lua
+++ nvim/lua/plugins/cheatsheet.lua
@@ -1,6 +1,9 @@
 return {
   "doctorfree/cheatsheet.nvim",
   event = "VeryLazy",
+  keys = {
+    { "<localleader>?", ":<C-U>Cheatsheet<CR>", desc = "Cheatsheet" },
+  },
   dependencies = {
     { "nvim-telescope/telescope.nvim" },
     { "nvim-lua/popup.nvim" },
