a5cf17c00f4d1c0269fc0eaefcb6691ddbc1f330
Christian C. Berclaz <christian.berclaz@mac.com>
2024-11-13 11:39:13 +0100

    CHANGED: Added shortcuts for the Flote plugin.

--- nvim/lua/plugins/flote.lua
+++ nvim/lua/plugins/flote.lua
@@ -1,23 +1,27 @@
 return {
   "JellyApple102/flote.nvim",
-  config = function()
-    require("flote").setup({
-      q_to_quit = true,
-      window_style = "minimal",
-      window_border = "solid",
-      window_title = true,
-      notes_dir = vim.fn.stdpath("cache") .. "/flote",
-      files = {
-        global = "flote-global.md",
-        cwd = function()
-          return vim.fn.getcwd()
-        end,
-        file_name = function(cwd)
-          local base_name = vim.fs.basename(cwd)
-          local parent_base_name = vim.fs.basename(vim.fs.dirname(cwd))
-          return parent_base_name .. "_" .. base_name .. ".md"
-        end,
-      },
-    })
-  end,
+  opts = {
+    q_to_quit = true,
+    window_style = "minimal",
+    window_border = "solid",
+    window_title = true,
+    notes_dir = vim.fn.stdpath("cache") .. "/flote",
+    files = {
+      global = "flote-global.md",
+      cwd = function()
+        return vim.fn.getcwd()
+      end,
+      file_name = function(cwd)
+        local base_name = vim.fs.basename(cwd)
+        local parent_base_name = vim.fs.basename(vim.fs.dirname(cwd))
+        return parent_base_name .. "_" .. base_name .. ".md"
+      end,
+    },
+  },
+  keys = {
+    { "<leader>bf", ":<C-U>Flote<CR>", desc = "Flote local" },
+    { "<leader>F", "", desc = "Flote" },
+    { "<leader>Fg", ":<C-U>Flote global<CR>", desc = "Flote global" },
+    { "<leader>Fm", ":<C-u>Flote manage<CR>", desc = "Flote manage" },
+  },
 }
