2605c1c42536cfd7dd2a420730ed619fd5231cd0
Christian C. Berclaz <christian.berclaz@mac.com>
2024-11-18 22:58:16 +0100

    feat(nvim): Added a working setup for the Snacks.dashboard

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -8,7 +8,47 @@ return {
     quickfile = { enabled = true },
     words = { enabled = true },
     statuscolumn = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      -- {
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          indent = 2,
+          padding = 1,
+          height = 6,
+        },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1, height = 6 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            local handle = io.popen("git rev-parse --is-inside-work-tree 2>&1")
+            if handle then
+              local result = handle:read("*a") or ""
+              print("result:", "'" .. result .. "'")
+              handle:close()
+              return result == "true\n"
+            end
+            return false
+          end,
+          -- vim.fn.isdirectory(".git") == 1,
+          cmd = "hub status --short --branch --renames",
+          height = 6,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = "startup" },
+      },
+      -- },
+    },
     styles = {
       notification = {
         wo = { wrap = true }, -- Wrap notifications
