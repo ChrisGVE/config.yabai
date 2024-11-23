3576ed4f9dc724544cc413817d1ca37693184ff6
Christian C. Berclaz <christian.berclaz@mac.com>
2024-11-23 13:42:17 +0100

    feat(nvim): added languages and mason tool installer

---
+++ nvim/lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,31 @@
+return {
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  opts = {
+    ensure_installed = {
+      "alex", -- text
+      "beautysh", -- shells
+      "djlint", -- Jinja
+      "esbonio", -- Sphinx
+      "jinja-lsp", -- Jinja
+      "lemminx", -- XML
+      "ltex-ls", -- Latex
+      "prettierd", -- formatter
+      "selene", -- lua
+      "sonarlint-language-server", -- multiple, selected for XML
+      "swiftlint", -- Swift
+      "textlint", -- text
+      "textlsp", -- Text
+      "typos", -- Text
+      "typos-lsp", -- Text
+      "vale", -- text
+      "vale-ls", -- Text
+      "vim-language-server", -- VimScript
+      "vint", -- VimScript
+      "woke", -- Text
+      "write-good", -- text
+      "xmlformatter", -- XML
+    },
+    auto_update = true,
+    debounce_hours = 12, -- at least 12 hours between attempts to install/update
+  },
+}
