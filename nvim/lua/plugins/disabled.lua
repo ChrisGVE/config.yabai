acd2ed065d9afe4f022d6dd522ffdc6a4a874813
Christian C. Berclaz <christian.berclaz@mac.com>
2024-11-23 14:28:15 +0100

    style(nvim): disabled mini.animate as it degrades performance

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,5 +2,6 @@ return {
   -- Disable plugins:
   { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
+  { "echasnovski/mini.animate", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
 }
